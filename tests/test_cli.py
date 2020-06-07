b856fa0d99e0944cb52ca80c4ab17aa0b29ecb8c
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2020-06-07 20:25:56 +0100

    add failing test for trying to use add-from-file on empty.md

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,12 @@
 """Test the CLI"""
+import pytest
 from apy.cli import main
 from click.testing import CliRunner
 import tempfile
 import shutil
 
-test_collection_dir = "tests/data/test_base"
+test_data_dir = "tests/data/"
+test_collection_dir = test_data_dir + "test_base/"
 
 def test_cli_base_directory():    
     runner = CliRunner()
@@ -17,4 +19,22 @@ def test_cli_base_directory():
         # ... and succeed when it is valid
         shutil.copytree(test_collection_dir, tmpdirname, dirs_exist_ok=True)
         result = runner.invoke(main, ["-b", tmpdirname])
+        assert result.exit_code == 0
+
+test_markdown_files = [
+    # add files here that apy should be able to use
+    # add-from-file on successfully
+    "basic.md",
+    "empty.md",
+]
+
+@pytest.mark.parametrize("notes_filename",
+                         [test_data_dir + i for i in test_markdown_files])
+def test_cli_add_from_file(notes_filename):
+    runner = CliRunner()
+    
+    with tempfile.TemporaryDirectory() as tmpdirname:      
+        shutil.copytree(test_collection_dir, tmpdirname, dirs_exist_ok=True)
+        result = runner.invoke(main, ["-b", tmpdirname, "add-from-file", notes_filename])
+        
         assert result.exit_code == 0
